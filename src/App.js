85c130d71509a64e780ab07d2b466f0b7e6f3c66
Silas Nordgren <nordgrensilas@gmail.com>
2019-05-07 14:22:21 +0200

    Add dynamic chart growth and questions-as-objects

--- src/App.js
+++ src/App.js
@@ -1,67 +1,141 @@
 import React from 'react';
 import {
-  Radar, RadarChart, PolarGrid, PolarAngleAxis,
+  Radar, RadarChart, PolarGrid, PolarAngleAxis, PolarRadiusAxis
 } from 'recharts';
 
-const max = 6;
-const data = [
+const max = 10;
+const values = {};
+
+const sumObjects = ix => {
+  const defaultValue = 1;
+  const obj = values[ix];
+
+  if (!obj) {
+    values[ix] = {};
+    return defaultValue;
+  }
+
+  let result = defaultValue;  
+  Object.values(obj).forEach(value => {
+    result += value;
+  });
+  return result;
+};
+
+const generateData = values => [
   {
-    subject: 'Tillväxtstarka ägare', A: 0, fullMark: max,
+    subject: 'Tillväxtstarka ägare', A: sumObjects(0), fullMark: max,
   },
   {
-    subject: 'Tillväxtfinansiering', A: 3, fullMark: max,
+    subject: 'Tillväxtfinansiering', A: sumObjects(1), fullMark: max,
   },
   {
-    subject: 'Tillväxtutrymmet', A: 2, fullMark: max,
+    subject: 'Tillväxtutrymmet', A: sumObjects(2), fullMark: max,
   },
   {
-    subject: 'Tidsmonopolet', A: 2, fullMark: max,
+    subject: 'Tidsmonopolet', A: sumObjects(3), fullMark: max,
   },
   {
-    subject: 'Fokus', A: 2, fullMark: max,
+    subject: 'Fokus', A: sumObjects(4), fullMark: max,
   },
   {
-    subject: 'Marknadsskapande', A: 2, fullMark: max,
+    subject: 'Marknadsskapande', A: sumObjects(5), fullMark: max,
   },
   {
-    subject: 'Internationell kraft', A: 2, fullMark: max,
+    subject: 'Internationell kraft', A: sumObjects(6), fullMark: max,
   },
   {
-    subject: 'Dynamisk effektivitet', A: 2, fullMark: max,
+    subject: 'Dynamisk effektivitet', A: sumObjects(7), fullMark: max,
   },
   {
-    subject: 'Hårdmjuka stårning', A: 2, fullMark: max,
+    subject: 'Hårdmjuka stårning', A: sumObjects(8), fullMark: max,
   },
   {
-    subject: 'Tältorganisation', A: 2, fullMark: max,
+    subject: 'Tältorganisation', A: sumObjects(9), fullMark: max,
   },
   {
-    subject: 'Tillväxtledarskapet', A: 2, fullMark: max,
+    subject: 'Tillväxtledarskapet', A: sumObjects(10), fullMark: max,
   },
   {
-    subject: 'Ledningskraften', A: 2, fullMark: max,
+    subject: 'Ledningskraften', A: sumObjects(11)
   }
 ];
 
-function App() {
+const questions = [
+  {
+    title: 'Är era ägare långsiktiga "företagsbyggare"?',
+    subjectIndex: 0,
+    formName: 'q0',
+    answers: [
+      {
+        text: 'Ja, erfaren ägare av att bygga tillväxtbolag (= förstår vad framtiden kräver)',
+        score: 3
+      },
+      {
+        text: 'Vissa, men de är ej i majoritet',
+        score: 2,
+      },
+      {
+        text: 'Nej, ägandet kortsiktigt, "exitfokus"',
+        score: 1
+      }
+    ]
+  },
+  {
+    title: 'Är era ägare långsiktiga "företagsbyggare"?',
+    subjectIndex: 1,
+    formName: 'q1',
+    answers: [
+      {
+        text: 'Ja, erfaren ägare av att bygga tillväxtbolag (= förstår vad framtiden kräver)',
+        score: 3
+      },
+      {
+        text: 'Vissa, men de är ej i majoritet',
+        score: 2,
+      },
+      {
+        text: 'Nej, ägandet kortsiktigt, "exitfokus"',
+        score: 1
+      }
+    ]
+  }
+];
+
+// Applies 
+const applyAnswer = (question, answer, reRender) => {
+  const previous = values[question.subjectIndex];
+  const toUse = (previous) ? previous : {};
+  if (!previous) {
+    values[question.subjectIndex] = toUse;
+  }
+  toUse[answer.formName] = answer.score;
+  console.log(values);
+  reRender();
+};
+
+const Question = (question, reRender) => {
+  return (
+    <div>
+      <h2>{question.title}</h2>
+      <form className="question-form">
+        {question.answers.map(answer => (
+          <div>
+            <input type="radio" name={question.formName} 
+              onClick={() => applyAnswer(question, answer, reRender)}/> 
+            {answer.text}
+          </div>
+        ))}
+      </form>
+    </div>
+  );
+}
+
+function App(reRender) {
   return (
     <div>
       <h1>LTU Business</h1>
-      <h2>Är era era ägare långsiktiga "företagsbyggare"?</h2>
-      <form className="question-form">
-        <div>
-          <input type="radio" name="q0" /> 
-          Ja, erfaren ägare av att bygga tillväxtbolag (= förstår vad framtiden kräver)
-        </div>
-        <div>
-          <input type="radio" name="q0" /> 
-          Vissa, men de är ej i majoritet
-        </div>
-        <div>
-          <input type="radio" name="q0" />
-          Nej, ägandet kortsiktigt, "exitfokus"
-        </div>
-      </form>
+      {questions.map(question => Question(question, reRender))}
       <Chart />
     </div>
   )
@@ -73,8 +147,11 @@ function Chart() {
   var height = 2.5 * radius;
 
   return (
-    <RadarChart outerRadius={radius} width={width} height={height} data={data}>
+    <RadarChart outerRadius={radius} width={width} height={height} 
+      data={generateData(values)} >
+
       <PolarGrid />
+      <PolarRadiusAxis domain={[0, max]} />
       <PolarAngleAxis dataKey="subject" />
       <Radar name="Result" dataKey="A" stroke="#7874c8" fill="#8884d8" fillOpacity={0.8} />
     </RadarChart>
